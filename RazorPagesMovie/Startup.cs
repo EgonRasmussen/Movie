5b170464065484ef0f21074c68989df6f4d7d491
ECR\Egon Rasmussen <ecr@live.dk>
2019-03-23 17:42:06 +0100

    ok

--- RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/Startup.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using DataLayer.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -26,16 +21,13 @@ namespace RazorPagesMovie
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
 
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
@@ -46,10 +38,9 @@ namespace RazorPagesMovie
             services.AddScoped<IMovieService, MovieService>();
             services.AddScoped<IGenreService, GenreService>();
 
-            services.AddAutoMapper(typeof(RazorPagesMovie.Startup));
+            services.AddAutoMapper();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -60,7 +51,6 @@ namespace RazorPagesMovie
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -70,7 +60,6 @@ namespace RazorPagesMovie
 
             app.UseMvc();
         }
-
     }
 
     public class MovieProfile : Profile
@@ -84,8 +73,6 @@ namespace RazorPagesMovie
                     .ForMember(dto => dto.GenreName, conf => conf.MapFrom(g => g.Genre.GenreName))
                     .ReverseMap();
             });
-
-                      
         }
     }
 }
